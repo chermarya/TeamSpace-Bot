83d14d41636cdb4582b408f3c1278a40c2ae8da4
chermarya <106978580+chermarya@users.noreply.github.com>
2024-12-28 17:53:57 +0200

    bot finished

--- project/main.py
+++ project/main.py
@@ -10,21 +10,25 @@ user_id = {}
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     bot.send_message(message.chat.id, "Hello! Welcome to TeamSpace Support Bot!")
-    send_email(message)
+    send_email(message, message.from_user.id)
 
 
-def send_email(message):
+@bot.message_handler(commands=['report'])
+def report_handler(message):
+    send_email(message, message.from_user.id)
+
+
+def send_email(message, user_id):
+    user_state[user_id] = {'state': 'WAITING_FOR_EMAIL'}
     bot.send_message(message.chat.id, "Please, enter your email address")
-    user_state[message.from_user.id] = {'state': 'WAITING_FOR_EMAIL'}
 
 
-@bot.message_handler(func=lambda message: message.from_user.id in user_state and 
-                                          user_state[message.from_user.id]['state'] == 'WAITING_FOR_EMAIL')
+@bot.message_handler(func=lambda message: user_state[message.from_user.id]['state'] == 'WAITING_FOR_EMAIL')
 def handle_email_input(message):
-    
+    user_state.pop(message.from_user.id)
     user_email = message.text
-    user = find_user_in_db(user_email)
-    user_state[message.from_user.id] = {'id': user[0]}
+    user = find_user_in_db(message, user_email)
+
     if not user == None:
         bot.send_message(message.chat.id, f"""
         <b>Find user:</b>
@@ -40,15 +44,32 @@ def handle_email_input(message):
         bot.send_message(message.chat.id, "Is this data correct?", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, f"Cannot find user with email \"{user_email}\"")
-        send_email(message)
+        send_email(message, message.from_user.id)
 
 
-@bot.message_handler(func=lambda message: message.from_user.id in user_state and
-                                          user_state[message.from_user.id]['state'] == 'WAITING_FOR_MESSAGE')
+@bot.message_handler(func=lambda message: user_state[message.from_user.id]['state'] == 'WAITING_FOR_MESSAGE')
 def handle_email_input(message):
     text = message.text
-    
+    id = user_id[message.from_user.id]['id']
+    try:
+        connection = psycopg2.connect(
+            dbname='d5t7e9lfmff5dr',
+            user='u851cr50ug5urc',
+            password='p322a81b96e38ad10bc7b3a8682014b13c7c370ddc89a4a4b89d9b64944ad099a',
+            host='clhtb6lu92mj2.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+            port='5432'
+        )
+        cursor = connection.cursor()
+        query = f"INSERT INTO reports (user_id, message_text) VALUES ({id}, '{text}')"
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        return 0
 
+    bot.send_message(message.chat.id, f"Your report has been sent successfully")
+    bot.send_message(message.chat.id, f"For sending a report use the command:\n/report")
 
 
 @bot.callback_query_handler(func=lambda callback: True)
@@ -59,10 +80,10 @@ def callback_handler(call):
         user_state[call.from_user.id] = {'state': 'WAITING_FOR_MESSAGE'}
     elif call.data == "wrong_user":
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-        send_email(call.message)
+        send_email(call.message, call.from_user.id)
 
 
-def find_user_in_db(email):
+def find_user_in_db(message, email):
     try:
         connection = psycopg2.connect(
             dbname='d5t7e9lfmff5dr',
@@ -78,6 +99,7 @@ def find_user_in_db(email):
         cursor.close()
         connection.close()
         if user is not None:
+            user_id[message.from_user.id] = {'id': user[0]}
             res = []
             for i in user:
                 res.append(i)
